69b22c821ed904886d1a32a3ee56f2a58c42ad08
Ryan Barker <celandro@gmail.com>
2017-04-15 22:01:43 -0700

    Minor fixes

--- src/main/java/com/pokebattler/fight/calculator/MonteCarloSimulator.java
+++ src/main/java/com/pokebattler/fight/calculator/MonteCarloSimulator.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.pokebattler.fight.data.PokemonDataCreator;
@@ -23,15 +25,16 @@ public class MonteCarloSimulator implements AttackSimulator {
     private PokemonRepository pokemonRepository;
     @Resource
     private IndividualSimulator simulator;
-    public static int NUM_SIMULATIONS=99;
+    @Value("${monteCarlo.numRounds}")
+    private int numRounds=999;
     @Resource
     OverallRankingsSort sort;
     
 
 	@Override
 	public Builder fight(Fight fight, boolean includeDetails) {
-		ArrayList<Builder> results = new ArrayList<>(NUM_SIMULATIONS);
-		for (int i=0; i< 99; i++) {
+		ArrayList<Builder> results = new ArrayList<>(numRounds);
+		for (int i=0; i< numRounds; i++) {
 			results.add(simulator.fight(fight, includeDetails));
 		}
 		Builder result = FightResult.newBuilder();
@@ -39,7 +42,7 @@ public class MonteCarloSimulator implements AttackSimulator {
 			// sort the results
 			results.sort(sort.getFightResultComparator());
 		}
-		Builder median = results.get((NUM_SIMULATIONS+1)/2);
+		Builder median = results.get((numRounds+1)/2);
 
 		result.setFightParameters(median.getFightParametersBuilder())
 				.setWin(median.getWin())
@@ -65,30 +68,26 @@ public class MonteCarloSimulator implements AttackSimulator {
 				CombatantResult.Builder resultCombatant = result.getCombatantsBuilderList().get(i);
 				resultCombatant.setCombatTime(resultCombatant.getCombatTime() + combatant.getCombatTime());
 				resultCombatant.setDamageDealt(resultCombatant.getDamageDealt() + combatant.getDamageDealt());
+				// store damage here
+				resultCombatant.setEndHp(resultCombatant.getEndHp() + (combatant.getStartHp() - combatant.getEndHp()));
 				resultCombatant.setEnergy(resultCombatant.getEnergy() + combatant.getEnergy());
 			}
 		});
-		result.setPower(Math.pow(10.0, result.getPower()));
-		result.setOverallRating(Math.pow(10.0, result.getOverallRating()));
-		results.stream().forEach(fightResult -> {
-			result.setEffectiveCombatTime(result.getEffectiveCombatTime() / NUM_SIMULATIONS);
-			result.setTotalCombatTime(result.getTotalCombatTime() / NUM_SIMULATIONS);
-			result.setPowerLog(result.getPowerLog() / NUM_SIMULATIONS);
-			result.setPower(Math.pow(10.0,result.getPowerLog()));
-			result.setPotions(result.getPotions() / NUM_SIMULATIONS);
-			result.setOverallRating(Math.pow(10.0,(result.getOverallRating()/NUM_SIMULATIONS)));
-			for (int i=0; i< fightResult.getCombatantsBuilderList().size(); i++) {
-				CombatantResult.Builder resultCombatant = result.getCombatantsBuilderList().get(i);
-				resultCombatant.setCombatTime(resultCombatant.getCombatTime() / NUM_SIMULATIONS);
-				resultCombatant.setDamageDealt(Math.round(resultCombatant.getDamageDealt() / NUM_SIMULATIONS));
-				resultCombatant.setEnergy(resultCombatant.getEnergy() / NUM_SIMULATIONS);
-				resultCombatant.setDps(resultCombatant.getDamageDealt() / (float) resultCombatant.getCombatTime());
-				resultCombatant.setEndHp(resultCombatant.getStartHp() - resultCombatant.getDamageDealt());
+		result.setEffectiveCombatTime(result.getEffectiveCombatTime() / numRounds);
+		result.setTotalCombatTime(result.getTotalCombatTime() / numRounds);
+		result.setPowerLog(result.getPowerLog() / numRounds);
+		result.setPower(Math.pow(10.0,result.getPowerLog()));
+		result.setOverallRating(Math.pow(10.0,(result.getOverallRating()/numRounds)));
+		result.setPotions(result.getPotions() / numRounds);
+		for (int i=0; i< result.getCombatantsBuilderList().size(); i++) {
+			CombatantResult.Builder resultCombatant = result.getCombatantsBuilderList().get(i);
+			resultCombatant.setCombatTime(resultCombatant.getCombatTime() / numRounds);
+			resultCombatant.setDamageDealt(Math.round(resultCombatant.getDamageDealt() / numRounds));
+			resultCombatant.setEnergy(resultCombatant.getEnergy() / numRounds);
+			resultCombatant.setDps(1000.0f * resultCombatant.getDamageDealt() / (float) resultCombatant.getCombatTime());
+			resultCombatant.setEndHp(resultCombatant.getStartHp() - (resultCombatant.getEndHp() / numRounds));
 
-			}			
-			
-		});
-		
+		}			
 		return result;
 	}
 
